0b8ec3cbd0c70ce0c8b2430034245275f9895183
Mathias Sant'Anna <dark_tricolor@hotmail.com>
2024-10-02 00:12:46 +0100

    initial commit for 2 more small projects

---
+++ Higher Lower Game/main/main.py
@@ -0,0 +1,5 @@
+word_per_page = 0
+pages = int(input("num of pages: "))
+word_per_page = int(input("num of words per page: "))
+total_word = pages * word_per_page
+print(total_word)
---
+++ Prime Numbers/main.py
@@ -0,0 +1,14 @@
+# Write your code below this line 👇
+def prime_checker(number):
+  is_prime = True
+  for i in range(2, number):
+    if number % i == 0:
+      is_prime = False
+  if is_prime:
+    print(f"It's a prime number.")
+  else:
+    print(f"It's not a prime number.")
+
+
+n = int(input()) # Check this number
+prime_checker(number=n)
